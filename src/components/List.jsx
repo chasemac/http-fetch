1ebe60d268df61c22f61652f17f9d64b8c68f6bd
Chase McElroy <ichasem@gmail.com>
2016-05-11 10:48:09 -0700

    Added Http fetch request to List site

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,11 +1,22 @@
 var React = require('react');
 var ListItem = require('./ListItem.jsx');
+var HTTP = require('../services/httpservice');
 
-var ingredients = [{"id":1,"text":"ham"}, {"id":2,"text":"cheese"},{"id":3,"text":"potatoes"}];
 
 var List = React.createClass({
+getInitialstate: function() {
+  return {ingredients:[]};
+},
+componentWillMount: function() {
+  HTTP.get('/ingredients')
+  .then(function(data) {
+    console.log("DATA: ", data);
+    this.setState({ingredients: data});
+  }.bind(this));
+},
+
     render: function() {
-        var listItems = ingredients.map(function(item) {
+        var listItems = this.state.ingredients.map(function(item) {
             return <ListItem key={item.id} ingredient={item.text} />;
         });
 
