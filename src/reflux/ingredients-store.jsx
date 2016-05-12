3f108e145be892a103f9150772e017cfb55e1824
Chase McElroy <ichasem@gmail.com>
2016-05-11 18:00:03 -0700

    Set up React Reflux

---
+++ src/reflux/ingredients-store.jsx
@@ -0,0 +1,28 @@
+var HTTP = require('../services/httpservice');
+var Reflux = require('reflux');
+var Actions = require('./actions.jsx');
+
+var IngredientStore = Reflux.createStore({
+  listenables: [Actions],
+  getIngredients: function() {
+    HTTP.get('/ingredients')
+    .then(function(json) {
+      this.ingredients = json;
+      this.fireUpdate();
+
+    }.bind(this));
+
+  },
+  postIngredient: function(text) {
+// posted ingredient to server
+// now we got a successful callback
+
+  },
+// refresh function
+  fireUpdate: function() {
+    this.trigger('change', this.ingredients);
+  }
+
+});
+
+module.exports = IngredientStore;
